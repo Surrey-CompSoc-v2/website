ba2a323f03710dfa7f3ffd1a39cc796f65106001
rxghavc <r.commandur@gmail.com>
2026-04-15 12:17:16 +0100

    Fix merge conflict markers in manifests and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,11 +36,7 @@ export default function Navbar() {
               alt="Logo"
               width={100}
               height={100}
-<<<<<<< HEAD
-              style={{ height: "auto" }}
-=======
               style={{ width: "auto", height: "auto" }}
->>>>>>> origin/main
             />
           </Link>
         </div>
